0413d5616dea64ff469d1066e7a5376d08614147
JazyVain <renxuslan@gmail.com>
2025-08-12 16:42:06 -0300

    consertando

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,17 @@ const express = require("express")
 const fetch = require("fetch")
 const cors = require("cors")
 
+const app = express()
+const routes = require('./routes');
+app.use('/api', routes);
 
-giajo
+const port = 3000
+
+app.use(cors())
+
+app.listen(port, () => {
+  console.log(`Servidor de API está rodando na porta ${port}`);
+});
+
 
 //sempre colocar connection.end();
