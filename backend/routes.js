c43cf2ee47063ffc5e31c3db0eb2e72f7fea7304
JazzyVain <renxuslan@gmail.com>
2025-08-12 20:48:50 -0300

    Update routes.js

--- backend/routes.js
+++ backend/routes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const router = express.Router();  
