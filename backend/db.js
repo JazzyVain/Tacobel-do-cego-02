0070b48da89d218d7fcad8ef2a6fdbf56e902fb5
JazyVain <renxuslan@gmail.com>
2025-08-13 15:54:10 -0300

    To dando commit

--- backend/db.js
+++ backend/db.js
@@ -1,1 +1,13 @@
-const mysql = require("mysql2/promise");
+require('dotenv').config(); // Carrega variáveis do .env
+const mysql = require('mysql2/promise'); // Importa mysql2 na versão async/await
+
+// Cria um "pool" de conexões
+const db = mysql.createPool({
+  host: process.env.DB_HOST,     // Endereço do servidor MySQL
+  user: process.env.DB_USER,     // Usuário do MySQL
+  password: process.env.DB_PASSWORD, // Senha do usuário
+  database: process.env.DB_NAME      // Nome do banco
+  
+});
+
+module.exports = db; // Exporta a conexão pra usar nas rotas
