3fa6c39f3712e34e98bbce70894c025562982ed1
JazyVain <renxuslan@gmail.com>
2025-08-18 14:30:55 -0300

    Uauuuuu

--- backend/routes.js
+++ backend/routes.js
@@ -10,9 +10,11 @@ router.get('/', (req, res) => {
 })
 
 router.post('/response', (req, res) => {
-  res.json({mensagem: 'Olá'})
+  res.json({mensagem: 'Algo'})
 })
 
-
+router.post('/crunchwrap', (req, res) => {
+  res.json({mensagem: 'R$12,00'})
+})
 
 module.exports = router;
