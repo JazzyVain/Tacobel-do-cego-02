af628f37ee7d63e488df837dd2a99eb5765d6747
JazyVain <renxuslan@gmail.com>
2025-08-26 15:32:01 -0300

    Cadastro

--- backend/routes.js
+++ backend/routes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const db = require("./db")
+const db = require("./db");
+const bcrypt = require("bcryptjs")
 require("dotenv").config();
 
 const router = express.Router();  
@@ -32,20 +33,59 @@ router.get("/chalupa", async (req, res) => {
   }
 });
 
-router.post('/nacho', (req, res) => {
-  res.json({mensagem: 'R$10,95'})
-})
+router.get("/nacho", async (req, res) => {
+  try {
+    const [rows] = await db.query("SELECT preco from produtos where id = 3");
+    res.json(rows[0]); // retorna JSON
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
-router.post('/quesarito', (req, res) => {
-  res.json({mensagem: 'R$19,20'})
-})
+router.get("/quesarito", async (req, res) => {
+  try {
+    const [rows] = await db.query("SELECT preco from produtos where id = 4");
+    res.json(rows[0]); // retorna JSON
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
-router.post('/baja', (req, res) => {
-  res.json({mensagem: 'R$13,70'})
-})
+router.get("/baja", async (req, res) => {
+  try {
+    const [rows] = await db.query("SELECT preco from produtos where id = 5");
+    res.json(rows[0]); // retorna JSON
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
-router.post('/cinnamon', (req, res) => {
-  res.json({mensagem: 'R$8,20'})
-})
+router.get("/cinnamon", async (req, res) => {
+  try {
+    const [rows] = await db.query("SELECT preco from produtos where id = 6");
+    res.json(rows[0]); // retorna JSON
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.post("/cadastro", async (req, res) => {
+  const { nome, email, senha } = req.body;
+
+  if (!nome || !email || !senha) return res.status(400).json({ erro: "Campos obrigatórios" });
+
+  try {
+    const hash = await bcrypt.hash(senha, 10);
+    await db.query("INSERT INTO usuarios (nome, email, senha_hash) VALUES (?,?,?)", [
+      nome,
+      email,
+      hash
+    ]);
+    res.json({ sucesso: true });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ erro: "Erro ao salvar usuário" });
+  }
+});
 
 module.exports = router;
